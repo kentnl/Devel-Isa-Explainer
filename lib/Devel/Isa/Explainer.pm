01b4f847e5f03d1da193e8df18b3a02b7dd3b20e
Kent Fredric <kentfredric@gmail.com>
2016-03-31 20:44:00 +1300

    Build results of d0fbdc4 (on master)

--- lib/Devel/Isa/Explainer.pm
+++ lib/Devel/Isa/Explainer.pm
@@ -18,7 +18,7 @@ use MRO::Compat     ();
 
 BEGIN { *import = \&Exporter::import }    ## no critic (ProhibitCallsToUnexportedSubs)
 
-our @EXPORT = qw( explain_isa );          ## no critic (ProhibitAutomaticExportation)
+our @EXPORT_OK = qw( explain_isa );
 
 use constant 1.03 { map { ( ( sprintf '_E%x', $_ ), ( sprintf ' E<%s#%d>', __PACKAGE__, $_ ), ) } 1 .. 4 };
 
@@ -277,7 +277,7 @@ version 0.001000
 
 =head1 SYNOPSIS
 
-  use Devel::Isa::Explainer;
+  use Devel::Isa::Explainer qw( explain_isa );
 
   # Load it yourself first
   print explain_isa('Dist::Zilla::Dist::Builder');
