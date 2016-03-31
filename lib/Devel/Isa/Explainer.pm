3db55cd1b2f7659e5d57c76f13bd0759ec9df6a4
Kent Fredric <kentfredric@gmail.com>
2016-03-31 22:58:48 +1300

    Use (id: %Class#%Id)  instead of E<Class#Id> for error identifiers
    This is a bit easier to deal with in pod. But I'm not plussed with this
    :/
    
    <> is much more obvious and intuitive to me.

--- lib/Devel/Isa/Explainer.pm
+++ lib/Devel/Isa/Explainer.pm
@@ -20,7 +20,7 @@ BEGIN { *import = \&Exporter::import }    ## no critic (ProhibitCallsToUnexporte
 
 our @EXPORT_OK = qw( explain_isa );
 
-use constant 1.03 { map { ( ( sprintf '_E%x', $_ ), ( sprintf ' E<%s#%d>', __PACKAGE__, $_ ), ) } 1 .. 5 };
+use constant 1.03 { map { ( ( sprintf '_E%x', $_ ), ( sprintf ' (id: %s#%d)', __PACKAGE__, $_ ), ) } 1 .. 5 };
 
 {
   no strict 'refs';                       # namespace clean
