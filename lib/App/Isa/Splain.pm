e547fca57c106c64c273813f5a0d3cfd81da73f2
Kent Fredric <kentfredric@gmail.com>
2016-04-01 22:36:00 +1300

    v0.001001
     - Minor documentation fixes.

--- lib/App/Isa/Splain.pm
+++ lib/App/Isa/Splain.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package App::Isa::Splain;
 
-our $VERSION = '0.001001';
+our $VERSION = '0.001002';
 
 # AUTHORITY
 
--- lib/Devel/Isa/Explainer.pm
+++ lib/Devel/Isa/Explainer.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Devel::Isa::Explainer;
 
-our $VERSION = '0.001001';
+our $VERSION = '0.001002';
 
 # ABSTRACT: Pretty Print Hierarchies of Subs in Packages
 
